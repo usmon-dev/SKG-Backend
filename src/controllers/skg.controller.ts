79b7d7eb6c61192e4d66a535e738202122e269d9
Usmon <usmonW@icloud.com>
2024-09-01 12:53:30 +0500

    fixed date

--- src/controllers/skg.controller.ts
+++ src/controllers/skg.controller.ts
@@ -11,7 +11,6 @@ import {
   deleteDoc,
 } from "firebase/firestore";
 import { verifyToken } from "../utils/middleware";
-import { dateAndTime } from "../utils/defaults";
 
 interface SecretKey {
   id: string;
@@ -43,7 +42,7 @@ export const createSecretKey = [
       const { title } = req.body;
       const secretKey = crypto.randomBytes(32).toString("hex");
       const userId = req.userId;
-      const createdAt = dateAndTime;
+      const createdAt = new Date().toISOString();
       const newSecretKey = await addDoc(secretKeysCollection, {
         title,
         secretKey,
