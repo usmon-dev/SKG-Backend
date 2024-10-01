440bbbf15d916e8be09319ba440c74b063d429d4
Usmon <usmonW@icloud.com>
2024-10-01 18:04:14 +0500

    fixing date

--- src/utils/defaults.ts
+++ src/utils/defaults.ts
@@ -1,14 +1,14 @@
 import { DateTime } from 'luxon';
 
 // Specify your desired time zone
-const timezone = 'GMT+5'; // Replace with your desired time zone
+const timezone = 'UTC'; // Replace with your desired time zone
 
 // Get the current time in the specified time zone
 const now = DateTime.now().setZone(timezone);
 
 // Format the time and date
-const time = now.toFormat('DD.MM.YYYY HH:mm');
+const time = now.toFormat('HH:mm:ss');
 const date = now.toFormat('DD.MM.YYYY');
-const dateAndTime = now.toFormat('DD.MM.YYYY HH:mm');
+const dateAndTime = `${date} ${time}`;
 
 export { time, date, dateAndTime };
