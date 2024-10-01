8a4742d95f8a4c7e561ea8f607bac7b777012be0
Usmon <usmonW@icloud.com>
2024-10-01 18:01:58 +0500

    fixed date bug?

--- src/utils/defaults.ts
+++ src/utils/defaults.ts
@@ -1,14 +1,14 @@
-import { DateTime } from "luxon";
+import { DateTime } from 'luxon';
 
 // Specify your desired time zone
-const timezone = "GMT+5"; // Replace with your desired time zone
+const timezone = 'GMT+5'; // Replace with your desired time zone
 
 // Get the current time in the specified time zone
 const now = DateTime.now().setZone(timezone);
 
 // Format the time and date
-const time = now.toFormat("HH:mm:ss");
-const date = now.toFormat("DD.MM.YYYY");
-const dateAndTime = `${date} ${time}`;
+const time = now.toFormat('DD.MM.YYYY HH:mm');
+const date = now.toFormat('DD.MM.YYYY');
+const dateAndTime = now.toFormat('DD.MM.YYYY HH:mm');
 
 export { time, date, dateAndTime };
